7f0a61978a0b5fd2388c4e8e4faa50c466a58f14
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-01-07 14:31:06 -0600

    try some cross-compiling things

--- recipe/build-kelbt.sh
+++ recipe/build-kelbt.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
+
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  if [[ "${target_platform}" == linux-* ]]; then
+    # Get an updated config.sub and config.guess
+    # (see https://conda-forge.org/docs/maintainer/knowledge_base.html#cross-compilation)
+    pushd "${BUILD_PREFIX}/share/gnuconfig"
+      cp config.* "${SRC_DIR}"
+    popd
+  fi
+fi
+
 ./configure --prefix "${PREFIX}"
+
 make
 make check
 make install
