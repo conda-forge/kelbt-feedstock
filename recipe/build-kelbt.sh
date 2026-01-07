253ca53b28bb9875e48680d44709528342d8e641
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-01-07 15:23:28 -0600

    try -Wno-narrowing

--- recipe/build-kelbt.sh
+++ recipe/build-kelbt.sh
@@ -2,7 +2,11 @@
 set -eux -o pipefail
 
 if [[ "${host_platform}" == "linux-aarch64" || "${host_platform}" == "linux-ppc64le" ]]; then
+  # from https://conda-forge.org/docs/how-to/advanced/cross-compilation/
   cp "${BUILD_PREFIX}/share/gnuconfig"/config.* .
+  # avoid errors like...
+  #   klparse.cpp:163:17: error: narrowing conversion of '-1' from 'int' to 'char' [-Wnarrowing]
+  export CXXFLAGS="${CXXFLAGS} -Wno-narrowing"
 fi
 
 ./configure --prefix "${PREFIX}"
